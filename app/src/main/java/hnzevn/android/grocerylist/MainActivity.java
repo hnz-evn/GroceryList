4700df4fcd5ba788b50c810189d17a3c6fd2bc4b
Evan Hunzinger <hnz.evn@gmail.com>
2015-08-29 15:25:08 -0500

    Added a fragment which will contain the list of grocery items to choose from.

--- app/src/main/java/hnzevn/android/grocerylist/MainActivity.java
+++ app/src/main/java/hnzevn/android/grocerylist/MainActivity.java
@@ -1,6 +1,8 @@
 package hnzevn.android.grocerylist;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,5 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new ItemListFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
