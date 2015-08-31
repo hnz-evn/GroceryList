63b0a3fef6873b5cdf49773b0b98eae26d60511b
Evan Hunzinger <hnz.evn@gmail.com>
2015-08-30 23:25:05 -0500

    Changed default fragment back to GroceryPickerFragment and disabled the check box functionality on the list view.

--- app/src/main/java/hnzevn/android/grocerylist/activities/MainActivity.java
+++ app/src/main/java/hnzevn/android/grocerylist/activities/MainActivity.java
@@ -6,7 +6,6 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 
 import hnzevn.android.grocerylist.R;
-import hnzevn.android.grocerylist.fragments.GroceryListFragment;
 import hnzevn.android.grocerylist.fragments.GroceryPickerFragment;
 
 
@@ -21,7 +20,7 @@ public class MainActivity extends Activity {
         Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new GroceryListFragment();
+            fragment = new GroceryPickerFragment();
             fragmentManager.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
